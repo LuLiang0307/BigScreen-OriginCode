57add2b45f8ded625068be5e4cf4c41a73e23b4d
Lucia <2313970630@qq.com>
2022-10-15 16:31:05 +0800

    使用flex和grid布局

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,8 +4,15 @@ import headerBg from '../images/header.jpg'
 
 export const Home = () => {
   return (
-    <div>
-      <div className="x" style={{backgroundImage: `url(${headerBg})`}}></div>
+    <div className='home'>
+      <div className="header" style={{backgroundImage: `url(${headerBg})`}}></div>
+      <main>
+        <section className='section1'></section>
+        <section className='section2'></section>
+        <section className='section3'></section>
+        <section className='section4'></section>
+        <section className='section5'></section>
+      </main>
     </div>
   );
 };
