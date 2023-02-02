d733ef729c797284d3b64a5fd0db4dd126e0b3b0
Lucia <2313970630@qq.com>
2023-02-02 15:24:03 +0800

    重构chart1

---
+++ src/components/chart-1.tsx
@@ -0,0 +1,74 @@
+import React, { useDebugValue, useEffect, useRef } from 'react';
+import * as echarts from 'echarts';
+
+const px = (n) => n / 2420 * (window as any).pageWidth
+
+export const Chart1 = () => {
+  const divRef = useRef(null)
+  useEffect(() => {
+    const myEchart = echarts.init(divRef.current)
+    myEchart.setOption({
+      textStyle: {
+        fontSize: px(12)
+      },
+      xAxis: {
+        type: 'category',
+        data: ['城关区', '七里河区', '西固区', '安宁区', '红谷区', '永登区', '皋兰区', '榆中区', '兰州新区'],
+        axisLabel: {
+          fontSize: px(12),
+          margin: 3,
+          formatter(val) {
+            if (val.length > 2) {
+              //这里不能写成一行，返回的内容不正确
+              const array = val.split('')
+              array.splice(2, 0, '\n')
+              return array.join('')
+            } else {
+              return val
+            }
+          }
+        },
+        axisTick: {
+          show: false
+        },
+        axisLine: {
+          show: true,
+          lineStyle: {
+            color: '#0a5299'
+          }
+        },
+      },
+      grid: {
+        left: px(40),
+        right: px(40),
+        bottom: px(40),
+        top: px(40)
+      },
+      yAxis: {
+        axisLabel: {
+          fontSize: px(12),
+          margin:3
+        },
+        splitLine: {show: false},
+        axisLine: {
+          show: true,
+          lineStyle: {
+            color: '#0a5299'
+          }
+        },
+       
+      },
+      series: [{
+        type: 'bar',
+        data: [40, 30, 25, 18, 23, 14, 20, 28, 40]
+      }]
+    })
+  }, [])
+
+  return (
+    <div className='bordered 管辖统计'>
+    <h2>案发派出所管辖统计</h2>
+    <div ref={divRef} className='chart'></div>
+  </div>
+  )
+};
