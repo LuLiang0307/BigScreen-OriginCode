c1a80d666ee87c9d29b80d941210826852499710
Lucia <2313970630@qq.com>
2023-02-22 14:37:18 +0800

    布局chart7，8，9

---
+++ src/components/chart-7.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useRef } from 'react';
+import * as echarts from 'echarts';
+import { px } from '../shared/px';
+import china from '../geo/china.js';
+import { createEchartOptions } from '../shared/create-echart-options';
+
+export const Chart7 = () => {
+  const divRef = useRef(null);
+  useEffect(() => {
+    var myEchart = echarts.init(divRef.current);
+  }, []);
+
+  return (
+    <div className='年龄分布-图1'>
+      <div ref={divRef} className="chart" />
+      7
+    </div>
+  )
+}
